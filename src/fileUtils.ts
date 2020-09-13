15a72db8b7ac0aa6336911beb987ad1dcb9413aa
Schalk Neethling <volume4.schalk+github@gmail.com>
2020-09-13 19:11:46 +0200

    enable base template (#56)

---
+++ src/fileUtils.ts
@@ -0,0 +1,24 @@
+import klawSync from "klaw-sync";
+
+require("dotenv").config();
+
+const POSTS_ROOT = process.env.POSTS_ROOT;
+
+function getPosts() {
+  const allPaths = klawSync(POSTS_ROOT, { nodir: true });
+  let posts = [];
+
+  posts = allPaths.map((entry) => {
+    return {
+      path: entry.path,
+      fileName: entry.path.substring(
+        entry.path.lastIndexOf("/") + 1,
+        entry.path.lastIndexOf(".")
+      ),
+    };
+  });
+
+  return posts;
+}
+
+export { getPosts };

f87bb54d504765bbe68470e6b7cfc38da465c510
Schalk Neethling <sneethling@mozilla.com>
2022-01-26 20:41:09 +0200

    (fix:tests) fix by removing
    The index test is just not going to work. It seems testing the individual parts is going to be good enough. I then need to add some headless tests to cover the overall flow.

--- src/processors/sass-processor.ts
+++ src/processors/sass-processor.ts
@@ -13,21 +13,24 @@ export const processSASS = (tmpl) => {
   const $ = cheerio.load(tmpl, { xmlMode: true });
   const outputFile = "public/css/main.css";
   const sassEntry = $("link[type='text/sass']");
-  const sassFile = sassEntry.attr("href");
 
-  try {
-    const result = sass.compile(sassFile, {
-      style: outputStyle,
-      sourceMap: sourceMap,
-    });
+  if (sassEntry.length) {
+    try {
+      // for now, there will only ever be one sass entry
+      const sassFile = sassEntry[0].attribs.href;
+      const result = sass.compile(sassFile, {
+        style: outputStyle,
+        sourceMap: sourceMap,
+      });
 
-    fse.outputFileSync(outputFile, result.css, "utf8");
+      fse.outputFileSync(outputFile, result.css, "utf8");
 
-    sassEntry.attr("href", outputFile.replace("public", ".."));
-    sassEntry.attr("type", "text/css");
+      sassEntry.attr("href", outputFile.replace("public", ".."));
+      sassEntry.attr("type", "text/css");
 
-    return $.html();
-  } catch (error) {
-    console.error(`Error while processing SASS: ${error.toString()}`);
+      return $.html();
+    } catch (error) {
+      console.error(`Error while processing SASS: ${error.toString()}`);
+    }
   }
 };
